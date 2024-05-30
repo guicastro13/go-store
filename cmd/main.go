c725b13fca27a0c1cdfd13847735b94c5d536fe6
Guilherme Castro <guii_1@hotmail.com>
2024-05-30 00:41:29 -0300

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,28 @@ import (
   "github.com/guicastro13/go-store/config/logger"
 )
 
+type user struct {
+  Name string `json:"name"`
+  Age int `json:"age"`
+  Password string `json:"password"`
+}
+
+func (u user) LogUser() slog.Value {
+  return slog.GroupValue(
+    slog.String("name", u.Name),
+    slog.Int("age", u.Age),
+    slog.String("password", "HIDDEN"),
+  )
+}
+
 func main() {
   logger.InitLogger()
+  user := user{
+    Name: "Guilherme",
+    Age: 29,
+    Password: "123456",
+  }
 
   slog.Info("starting api")
+  slog.Info("creating user", "user", user.LogUser())
 }
