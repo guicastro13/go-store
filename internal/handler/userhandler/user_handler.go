5c15922446901467161b98363afbbab8785e3d23
Guilherme Castro <guii_1@hotmail.com>
2024-06-03 12:55:27 -0300

    create service

--- internal/handler/userhandler/user_handler.go
+++ internal/handler/userhandler/user_handler.go
@@ -51,6 +51,12 @@ func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) {
 	err = h.service.CreateUser(r.Context(), req)
 	if err != nil {
 		slog.Error(fmt.Sprintf("error to create user: %v", err), slog.String("package", "userhandler"))
+    if err.Error() == "cep not found" {
+      w.WriteHeader(http.StatusNotFound)
+      msg := httperr.NewNotFoundError("cep not found")
+      json.NewEncoder(w).Encode(msg)
+      return
+    }
 		w.WriteHeader(http.StatusInternalServerError)
 		msg := httperr.NewBadRequestError("error to create user")
 		json.NewEncoder(w).Encode(msg)
@@ -115,7 +121,18 @@ func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request) {
 	err = h.service.UpdateUser(r.Context(), req, id)
 	if err != nil {
 		slog.Error(fmt.Sprintf("error to update user: %v", err), slog.String("package", "userhandler"))
-		w.WriteHeader(http.StatusInternalServerError)
+    if err.Error() == "user not found" {
+      w.WriteHeader(http.StatusNotFound)
+      msg := httperr.NewNotFoundError("user not found")
+      json.NewEncoder(w).Encode(msg)
+      return
+    } else if err.Error() == "cep not found" {
+      w.WriteHeader(http.StatusNotFound)
+      msg := httperr.NewNotFoundError("cep not found")
+      json.NewEncoder(w).Encode(msg)
+      return
+    }
+		w.WriteHeader(http.StatusBadRequest)
 		msg := httperr.NewBadRequestError("error to update user")
 		json.NewEncoder(w).Encode(msg)
 		return
--- internal/service/userservice/user_service.go
+++ internal/service/userservice/user_service.go
@@ -3,11 +3,11 @@ package userservice
 import (
 	"context"
 	"errors"
-	"fmt"
 	"log/slog"
 	"time"
 
 	"github.com/google/uuid"
+	viacep "github.com/guicastro13/go-store/api"
 	"github.com/guicastro13/go-store/internal/dto"
 	"github.com/guicastro13/go-store/internal/entity"
 	"github.com/guicastro13/go-store/internal/handler/response"
@@ -29,11 +29,24 @@ func (s *service) CreateUser(ctx context.Context, u dto.CreateUserDto) error {
     slog.Error("error to encrypt password", "err", err, slog.String("package", "userservice"))
     return errors.New("error to escrypt password")
   }
+  cep, err := viacep.GetCep(u.CEP)
+  if err != nil {
+    slog.Error("error to get cep", "err", err, slog.String("package", "userservice"))
+    return err
+  }
   newUser := entity.UserEntity{
     ID: uuid.New().String(),
     Name: u.Name,
     Email: u.Email,
     Password: string(passwordEcrypted),
+    Address: entity.UserAddress{
+      CEP: cep.CEP,
+      IBGE: cep.IBGE,
+      UF: cep.UF,
+      City: cep.Localidade,
+      Complement: cep.Complemento,
+      Street: cep.Logradouro,
+    },
     CreatedAt: time.Now(),
     UpdatedAt: time.Now(),
   }
@@ -44,44 +57,126 @@ func (s *service) CreateUser(ctx context.Context, u dto.CreateUserDto) error {
   }
   return nil
 }
+
 func (s *service) UpdateUser(ctx context.Context, u dto.UpdateUserDto, id string) error {
+  var updateUser entity.UserEntity
+  if u.Email != "" {
+    verifyUserEmail, err := s.repo.FindUserByEmail(ctx, u.Email)
+    if err != nil {
+      slog.Error("error to search user by email", "err", err, slog.String("package", "userservice"))
+      return err
+    }
+    if verifyUserEmail != nil {
+      slog.Error("user already exists", slog.String("package", "userservice"))
+      return errors.New("user already exists")
+    }
+    updateUser.Email = u.Email
+  }
+  if u.CEP != "" {
+    cep, err := viacep.GetCep(u.CEP)
+    if err != nil {
+      slog.Error("error to get cep", "err", err, slog.String("package", "userservice"))
+      return err
+    }
+    updateUser.Address = entity.UserAddress{
+      CEP:        cep.CEP,
+      IBGE:       cep.IBGE,
+      UF:         cep.UF,
+      City:       cep.Localidade,
+      Complement: cep.Complemento,
+      Street:     cep.Logradouro,
+    }
+  }
+  return nil
+}
+
+func (s *service) GetUserByID(ctx context.Context, id string) (*response.UserResponse, error) {
+  userExists, err := s.repo.FindUserById(ctx, id)
+  if err != nil {
+    slog.Error("error to search user by id", "err", err, slog.String("package", "userservice"))
+    return nil, err
+  }
+  if userExists == nil {
+    slog.Error("user not found", slog.String("package", "userservice"))
+    return nil, errors.New("user no found")
+  }
+  user := response.UserResponse{
+    ID: userExists.ID,
+    Name: userExists.Name,
+    Email: userExists.Email,
+    CreatedAt: userExists.CreatedAt,
+    UpdatedAt: userExists.UpdatedAt,
+  }
+  return &user, nil
+}
+
+func (s *service) DeleteUser(ctx context.Context, id string) error {
+  userExists, err := s.repo.FindUserById(ctx, id)
+  if err != nil {
+    slog.Error("error to seach user by id", "err", err, slog.String("package", "userservice"))
+    return err
+  }
+  if userExists == nil {
+    slog.Error("user not found", slog.String("package", "userservice"))
+    return errors.New("user not found")
+  }
+  err = s.repo.DeleteUser(ctx, id)
+  if err != nil {
+    slog.Error("error to delete user", "err", err, slog.String("package", "userservice"))
+    return err
+  }
+  return nil
+}
+
+func (s *service) FindManyUsers(ctx context.Context) (*response.ManyUsersReponse, error) {
+  findManyUsers, err := s.repo.FindManyUsers(ctx)
+  if err != nil {
+    slog.Error("error to find many users", "err", err, slog.String("package", "userservice"))
+    return nil, err
+  }
+  users := response.ManyUsersReponse{}
+  for _, user := range findManyUsers{
+    userResponse := response.UserResponse{
+      ID: user.ID,
+      Name: user.Name,
+      Email: user.Email,
+      CreatedAt: user.CreatedAt,
+      UpdatedAt: user.UpdatedAt,
+    }
+    users.Users = append(users.Users, userResponse)
+  }
+  return &users, nil
+}
+
+func (s *service) UpdateUserPassword(ctx context.Context, u *dto.UpdateUserPasswordDto, id string) error {
   userExists, err := s.repo.FindUserById(ctx, id)
   if err != nil {
     slog.Error("error to search user by id", "err", err, slog.String("package", "userservice"))
     return err
   }
   if userExists == nil {
-    slog.Error("user not fund", slog.String("package", "userservice"))
-    return errors.New("user already exists")
+    slog.Error("user not found", slog.String("package", "userservice"))
+    return errors.New("user not found")
+  }
+  err = bcrypt.CompareHashAndPassword([]byte(userExists.Password), []byte(u.OldPassword))
+  if err != nil {
+    slog.Error("invalid password", slog.String("package", "userservice"))
+    return errors.New("invalid password")
+  }
+  err = bcrypt.CompareHashAndPassword([]byte(userExists.Password), []byte(u.Password))
+  if err == nil {
+    slog.Error("new password is equal to old password", slog.String("package", "userservice"))
+    return errors.New("new password is equal to old password")
+  }
+  passwordEncrypted, err := bcrypt.GenerateFromPassword([]byte(u.Password), 12)
+  if err != nil {
+    slog.Error("error to encrypt password", "err", err, slog.String("package", "userservice"))
+    return errors.New("error to encrypt password")
+  }
+  err = s.repo.UpdatePassword(ctx, string(passwordEncrypted), id)
+  if err != nil {
+    slog.Error("error to update password", "err", err, slog.String("package", "userservice"))
+    return err
   }
   return nil
-}
-
-func (s *service) GetUserByID(ctx context.Context, id string) (*response.UserResponse, error) {
-   return nil
-}
-
-func (s *service) DeleteUser(ctx context.Context, id string) error {
-	return nil
-}
-
-func (s *service) FindManyUsers(ctx context.Context) (response.ManyUsersReponse, error) {
-	usersFake := response.ManyUsersReponse{}
-	for i := 0; i < 5; i++ {
-		userFake := response.UserResponse{
-			ID:        "123",
-			Name:      "Guilherme",
-			Email:     fmt.Sprintf("guii_%v@hotmail.com", i),
-			CreatedAt: time.Now(),
-			UpdatedAt: time.Now(),
-		}
-		usersFake.Users = append(usersFake.Users, userFake)
-	}
-	return usersFake, nil
-}
-
-func (s *service) UpdateUserPassword(ctx context.Context, u *dto.UpdateUserPasswordDto, id string) error {
-	fmt.Println("new password: ", u.Password)
-	fmt.Println("old password: ", u.OldPassword)
-	return nil
 }
